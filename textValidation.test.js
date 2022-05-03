23eef3fc1e12c7d87755fab88fdaa30f2d39306f
Stephanie Jones <stephanieruthjones@gmail.com>
2022-05-03 17:47:54 -0600

    Update textValidation.test.js file

--- textValidation.test.js
+++ textValidation.test.js
@@ -1,8 +1,9 @@
 const { generateText, validateText, validateAndReturn } = require('./textValidation')
 
-//Unit Test bc only testing one function 
-//with input and output that does not use any other functions
-
+//Unit Test are fully isolated (e.g., testing one function)
+//generateText works with input and output that does not use any other functions
+//Unit tests ensure that individual components of the app work as expected. Assertions test the component API.
+//WRITE LOTS OF THESE
 describe('generateText works', () => {
     it('outputs dog name and breed', () => {
         const dogName = 'Ted'
@@ -25,8 +26,10 @@ describe('validateText works', () => {
         expect(validateText(text)).toBe(false)
     })
 })
-
-//Integration test 
+ 
+//Integration tests ensure that component collaborations work as expected.
+//e.g., testing a function that calls a function (or has dependencies)
+//WRITE A COUPLE OF THESE WHERE YOU GET A LOT OF BANG FOR YOUR BUCK
 describe('validateAndReturn works', () => {
     it('outputs dog name and breed', () => {
         const dogText = 'Melvin'
@@ -34,4 +37,7 @@ describe('validateAndReturn works', () => {
         expect(validateAndReturn(dogText, breed)).toBe('Melvin is a Terrier')
     })
 })
+
+//End-to-End test full flow, validating the DOM after a user click
+//WRITE A FEW OF THESE
 
